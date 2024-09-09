7c727a20c36aaaab67f0e7c7ce2ebfe93918f3a5
Tabish Bin Ishfaq <tabishbinishfaq1122@gmail.com>
2024-09-09 08:21:45 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ProjectsShowCase from "../components/sections/ProjectsShowCase";
+import React, { Suspense } from "react";
 import Hero from "../components/sections/Hero";
 import Header from "../components/sections/Header";
 import About from "../components/sections/About";
 import Experience from "../components/sections/Experience";
 import Skills from "../components/sections/Skills";
 import Education from "../components/sections/Education";
-import Projects from "../components/sections/Projects";
 import Contact from "../components/sections/Contact";
+
+const ProjectsShowCase = React.lazy(() =>
+  import("../components/sections/ProjectsShowCase")
+);
+const Projects = React.lazy(() => import("../components/sections/Projects"));
 
 const Home = () => {
   return (
@@ -26,8 +29,12 @@ const Home = () => {
         <Skills />
       </section>
       <section id="projects">
-        <ProjectsShowCase />
-        <Projects />
+        <Suspense fallback={<div>Loading Projects Showcase...</div>}>
+          <ProjectsShowCase />
+        </Suspense>
+        <Suspense fallback={<div>Loading Projects...</div>}>
+          <Projects />
+        </Suspense>
       </section>
       <section id="education">
         <Education />

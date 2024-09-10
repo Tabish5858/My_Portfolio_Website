2e787c0e44aa3609c823d43b7c0f710c64bbddae
Tabish Bin Ishfaq <tabishbinishfaq1122@gmail.com>
2024-09-10 08:50:35 +0500

    responsive updated

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -4,9 +4,12 @@ import experienceAnimation from "../../assets/images/lottie/coding.json";
 
 const About = () => {
   return (
-    <section className="About" >
+    <section className="About">
       <div className="container">
-        <div className="row gy-md-0 gy-4">
+        <div className="d-flex justify-content-center align-items-center mx-auto text-center heading-with-lines">
+          <h5>About Me</h5>
+        </div>
+        <div className="row gy-md-0 gy-4 pt-5">
           <div className="col-md-6">
             <div className="d-flex flex-column gap-lg-4 gap-3 text-md-start text-center justify-content-center h-100">
               <h4>WHO I AM?</h4>
